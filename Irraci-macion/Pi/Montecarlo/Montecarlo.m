4bc84b364ed095bbd7ab3137630d2f6d3b546b4d
Miguel Sánchez Palomino <miguelsanchez1703@gmail.com>
2019-05-07 20:17:49 +0200

    Añadido el archivo de aproximación a Montecarlo

---
+++ Irraci-macion/Pi/Montecarlo/Montecarlo.m
@@ -0,0 +1,18 @@
+pos = [-1 -1 2 2];
+rectangle('Position',pos,'Curvature',[1 1])
+axis equal
+pos = [-1 -1 2 2];
+rectangle('Position',pos)
+inside=0;
+total=0;
+for i=0:1:10000
+  posX = -1 +(1+1)*rand(1,1);
+  posY = -1+(1+1)*rand(1,1);
+  hold on;
+  plot([posX],[posY],'r.')
+  total+=1;
+  if (power(posX,2)+power(posY,2)<=1)
+    inside+=1;
+  endif
+endfor
+PI=4*(inside/total)
