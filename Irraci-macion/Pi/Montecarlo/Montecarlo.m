7a2953d3df07ac00dcfee4ad039aad30a0316e0b
Miguel Sánchez Palomino <miguelsanchez1703@gmail.com>
2019-05-08 12:58:10 +0200

    Optimizacion del codigo

--- Irraci-macion/Pi/Montecarlo/Montecarlo.m
+++ Irraci-macion/Pi/Montecarlo/Montecarlo.m
@@ -2,17 +2,29 @@ pos = [-1 -1 2 2];
 rectangle('Position',pos,'Curvature',[1 1])
 axis equal
 pos = [-1 -1 2 2];
+hold off;
 rectangle('Position',pos)
 inside=0;
 total=0;
-for i=0:1:10000
-  posX = -1 +(1+1)*rand(1,1);
-  posY = -1+(1+1)*rand(1,1);
-  hold on;
-  plot([posX],[posY],'r.')
-  total+=1;
-  if (power(posX,2)+power(posY,2)<=1)
-    inside+=1;
-  endif
+points=[];
+n=10000;
+posX=-1+2*rand(1,n);
+posY=-1+2*rand(1,n);
+Mods=posY.^2+posX.^2;
+for i=1:n
+        %posX = -1 +(1+1)*rand(1,1);
+        %posY = -1+(1+1)*rand(1,1);
+        %points=[points: posX posY];
+        %total+=1;
+        %if (power(posX,i)+power(posY,i)<=1)
+        if (Mods(1,i)<=1)
+                inside+=1;
+        endif
 endfor
-PI=4*(inside/total)
+hold on;
+%PI=4*(inside/total)
+format long g
+PI=4*(inside/n)
+%plot([points(:,1)],[points(:,2)],'ro')
+plot(posX,posY,'r.')
+axis equal
