2b5a79b311db57396726471a8e4c96042db9d342
Miguel Sánchez Palomino <miguelsanchez1703@gmail.com>
2019-05-08 17:24:21 +0200

    Inicio del codigo experimental Buffon

---
+++ Irraci-macion/Pi/Buffon/Experimental/Buffon.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#define longitud 6.6
+#define distancia 5
+
+using namespace std;
+
+int main () {
+	int palillos;
+	int cruces;
+	cout << "Cantidad de palillos: ";
+	cin >> palillos;
+	cout << "Cantidad de cruces: ";
+	cin >> cruces;
+	pi = (2*palillosTotal*longitud)/(cruces*distancia);
+}
