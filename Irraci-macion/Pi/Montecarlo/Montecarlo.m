0fe165eb86e98546c8ec1ae661253303c089d79e
Miguel Sánchez Palomino <miguelsanchez1703@gmail.com>
2019-05-08 17:15:36 +0200

    Añadido el input + eliminar comentarios

--- Irraci-macion/Pi/Montecarlo/Montecarlo.m
+++ Irraci-macion/Pi/Montecarlo/Montecarlo.m
@@ -7,24 +7,18 @@ rectangle('Position',pos)
 inside=0;
 total=0;
 points=[];
-n=10000;
+printf("Cantidad de iteraciones: ");
+n=input(' ');
 posX=-1+2*rand(1,n);
 posY=-1+2*rand(1,n);
 Mods=posY.^2+posX.^2;
 for i=1:n
-        %posX = -1 +(1+1)*rand(1,1);
-        %posY = -1+(1+1)*rand(1,1);
-        %points=[points: posX posY];
-        %total+=1;
-        %if (power(posX,i)+power(posY,i)<=1)
         if (Mods(1,i)<=1)
                 inside+=1;
         endif
 endfor
 hold on;
-%PI=4*(inside/total)
 format long g
 PI=4*(inside/n)
-%plot([points(:,1)],[points(:,2)],'ro')
 plot(posX,posY,'r.')
 axis equal
